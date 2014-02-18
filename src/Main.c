2c71f42cd05f50c4f16f123c2707094e96ba69b6
KyO <yong931231@hotmail.com>
2014-02-18 22:32:52 +0800

    Add a Main.c file to try BubbleSort. It should able to input and output the value of array without sorting.

---
+++ src/Main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "descendingBubbleSort.h"
+
+int main(void){
+
+	int size = 5;
+	int value, count;
+	int intArray[5];
+	printf("This is an descending BubbleSort program.\n");
+	printf("-----------------------------------------\n");
+	printf("You have an array with size of %i.\n\n", size);
+	
+	for(count = 0; count < size; count++){
+		printf("Please enter value into array of index %i :",count);
+		scanf("%i",&value);
+		intArray[count] = value;
+	}
+	
+	printf("\nHere is you result : \n");
+	printf("---------------------\n");
+	
+	for(count = 0; count < size; count++){
+		if(count == (size-1)){
+			printf("%i ",intArray[count]);
+			break;
+		}
+		printf("%i -> ",intArray[count]);
+	}
+	return 0;
+}
